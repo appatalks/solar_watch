94fa4d4598e686e9ee27eb04b319e397b1927d82
Steven <servingbaby@users.noreply.github.com>
2021-10-31 10:36:44 -0500

    Update solar_watch.sh

--- solar_watch.sh
+++ solar_watch.sh
@@ -3,6 +3,7 @@
 # solar_watch.sh - Monitors a web page for changes
 
 while true; do
+# if curl -s https://services.swpc.noaa.gov/products/alerts.json | egrep -i 'extreme|severe' > /dev/null
 if curl -s https://services.swpc.noaa.gov/products/alerts.json | grep -i 'extreme' > /dev/null
  then
         echo "Alert * Alert * Alert * Extreme Solar Storm and Radiation Detected * Take Action Now" > /var/www/vhosts/mywebsite/public_html/solar_watch.txt
