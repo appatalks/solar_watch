db8510305d00694b9a4b48858f4104c63c4d162c
servingbaby <charon_isis@hotmail.com>
2022-08-21 10:10:19 -0500

    limit curl to abt 3 days of data

--- solar_watch.sh
+++ solar_watch.sh
@@ -3,8 +3,8 @@
 # solar_watch.sh - Monitors a web page for changes
 
 while true; do
-# if curl -s https://services.swpc.noaa.gov/products/alerts.json | egrep -i 'extreme|severe' > /dev/null
-if curl -s https://services.swpc.noaa.gov/products/alerts.json | grep -i 'extreme' > /dev/null
+# Just grab the last 2-3 days of JSON Output.
+if curl -s https://services.swpc.noaa.gov/products/alerts.json | jq | head -n 25 | grep -i 'extreme' > /dev/null
  then
         echo "Alert * Alert * Alert * Extreme Solar Storm and Radiation Detected * Take Action Now" > /var/www/vhosts/mywebsite/public_html/solar_watch.txt
  else
