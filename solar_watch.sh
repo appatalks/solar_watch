060eadcc1b9ad64cf63a3318df8b6c067aa1c900
servingbaby <charon_isis@hotmail.com>
2022-08-21 20:34:27 -0500

    parse json

--- solar_watch.sh
+++ solar_watch.sh
@@ -3,6 +3,14 @@
 # solar_watch.sh - Monitors a web page for changes
 
 while true; do
+
+# Import All
+product_id=`curl -s https://services.swpc.noaa.gov/products/alerts.json | jq -r .[].product_id|head -n1`
+issue_datetime=`curl -s https://services.swpc.noaa.gov/products/alerts.json | jq -r .[].issue_datetime|head -n1`
+scale=`curl -s https://services.swpc.noaa.gov/products/alerts.json | jq -r .[].message |head -n15| grep "NOAA Scale"|head -n1`
+# Output to CSV for record keeping
+echo $issue_datetime,$cale >> Solar_watch_results.csv
+
 # Just grab the last 2-3 days of JSON Output.
 if curl -s https://services.swpc.noaa.gov/products/alerts.json | jq | head -n 25 | grep -i 'extreme' > /dev/null
  then
